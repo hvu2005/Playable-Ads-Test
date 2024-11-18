373ddef3116c72113a4d34d192f352f7737c3b16
hvu2005 <hoangvu241205@gmail.com>
2024-11-18 18:54:23 +0700

    update object pooling

--- Assets/Script/ObjectPooling/ObjectPooling.cs
+++ Assets/Script/ObjectPooling/ObjectPooling.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class ObjectPooling : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPref;
+    [SerializeField] private GameObject prefs;
     [SerializeField] private int poolSize;
-    private List<GameObject> bullets;
+    private List<GameObject> pool;
     // Start is called before the first frame update
     void Awake()
     {
-        bullets = new List<GameObject>();
+        pool = new List<GameObject>();
         for(int i = 0; i < poolSize; i++)
         {
-            GameObject bullet = Instantiate(bulletPref);
+            GameObject bullet = Instantiate(prefs);
             bullet.SetActive(false);
-            bullets.Add(bullet);
+            pool.Add(bullet);
         }
     }
-    // Update is called once per frame
-    void Update()
+    public GameObject GetObject()
     {
-        
+        foreach (GameObject obj in pool)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                obj.SetActive(true);
+                return obj;
+            }
+        }
+
+        GameObject newObj = Instantiate(prefs);
+        newObj.SetActive(true);
+        pool.Add(newObj);
+        return newObj;
+    }
+
+    public void ReturnObject(GameObject obj)
+    {
+        obj.SetActive(false); 
     }
 }
